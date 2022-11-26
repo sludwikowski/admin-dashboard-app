d7e3e6fa324ad0d0dc3de7b05c51d174aad57fee
Szymon  Ludwikowski <ludwikowskiszym1993@gmail.com>
2022-11-26 19:33:53 +0100

    `Sidebar` and `Header` components added

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,7 +1,13 @@
-import * as React from 'react'
+import React from 'react'
+import Header from '../../components/Header'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <Header
+      title={'DASHBOARD'}
+      subtitle={'Welcome to your dashboard'}
+    />
+  )
 }
 
 export default Dashboard
--- src/scenes/global/Topbar.js
+++ src/scenes/global/Topbar.js
@@ -1,5 +1,4 @@
-import * as React from 'react'
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 
 import { ColorModeContext, tokens } from '../../theme'
 import { Box, InputBase, IconButton, useTheme } from '@mui/material'
